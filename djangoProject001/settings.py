636020685e911320992f393c9324ca4d27cca5ae
taroun <aksghk4671@gmail.com>
2021-11-08 09:53:42 +0900

    11/08 env 풀기

--- djangoProject001/settings.py
+++ djangoProject001/settings.py
@@ -23,21 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR, '.env'))
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = 'django-insecure-%w*ih6ik17nd$v-c^bbds+6@h@(tv32s(aw6wrt2vrkzn=$@&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
